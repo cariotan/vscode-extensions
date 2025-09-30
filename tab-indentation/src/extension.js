2fbf97c9dad33fb313bc713441a39792d2cf70fe
cariotan <a@cario.me>
2025-09-30 14:59:27 +1000

    .

--- tab-indentation/src/extension.js
+++ tab-indentation/src/extension.js
@@ -1,50 +1,26 @@
-const vscode = require("vscode")
+let vscode = require("vscode")
 
 let applyingEdit = false
 
 function activate(context) {
-	const sub1 = vscode.workspace.onDidChangeTextDocument(onDocChange)
-	const sub2 = vscode.commands.registerTextEditorCommand("collapse-to-tab.fixDocument", fixWholeDocument)
-	context.subscriptions.push(sub1, sub2)
-}
-
-function onDocChange(e) {
-	if (applyingEdit) return
-
-	const editor = vscode.window.activeTextEditor
-	if (!editor) return
-	if (e.document.uri.toString() !== editor.document.uri.toString()) return
-
-	for (let i = 0; i < e.contentChanges.length; i++) {
-		const change = e.contentChanges[i]
-		if (change.text.length === 0) continue
-
-		const endPos = change.range.start.translate(0, change.text.length)
-		const startChar = Math.max(0, endPos.character - 4)
-		const startPos = new vscode.Position(endPos.line, startChar)
-		const last4Range = new vscode.Range(startPos, endPos)
-		const last4 = e.document.getText(last4Range)
-
-		if (last4 === "    " || last4 === "\t\t\t\t") {
-			applyingEdit = true
-			editor.edit(edit => {
-				edit.replace(last4Range, "\t")
-			}).finally(() => {
-				applyingEdit = false
-			})
-		}
-	}
+	let sub = vscode.commands.registerTextEditorCommand(
+		"collapse-to-tab.fixDocument",
+		fixWholeDocument
+	)
+	context.subscriptions.push(sub)
 }
 
 async function fixWholeDocument(editor) {
-	const doc = editor.document
-	const text = doc.getText()
-	const fullRange = new vscode.Range(doc.positionAt(0), doc.positionAt(text.length))
+	let doc = editor.document
+	let text = doc.getText()
+	let fullRange = new vscode.Range(doc.positionAt(0), doc.positionAt(text.length))
 
 	let replaced = text.replace(/\t{4}/g, "\t")
 	replaced = replaced.replace(/ {4}/g, "\t")
 
-	if (replaced === text) return
+	if (replaced === text) {
+		return
+	}
 
 	applyingEdit = true
 	await editor.edit(edit => {
@@ -53,6 +29,6 @@ async function fixWholeDocument(editor) {
 	applyingEdit = false
 }
 
-function deactivate() { }
+function deactivate() {}
 
 module.exports = { activate, deactivate }
