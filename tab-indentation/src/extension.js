01992e12c0dbfaada88593db47a9d6f1d0d15297
cariotan <a@cario.me>
2025-10-02 01:38:13 +1000

    .

--- tab-indentation/src/extension.js
+++ tab-indentation/src/extension.js
@@ -1,58 +1,39 @@
-const vscode = require("vscode")
-
-let applyingEdit = false
+let vscode = require("vscode")
 
 function activate(context) {
-	const sub1 = vscode.workspace.onDidChangeTextDocument(onDocChange)
-	const sub2 = vscode.commands.registerTextEditorCommand("collapse-to-tab.fixDocument", fixWholeDocument)
-	context.subscriptions.push(sub1, sub2)
-}
+	let disposable = vscode.commands.registerTextEditorCommand(
+		"collapse-to-tab.fixDocument",
+		async (editor) => {
+			if (!editor) {
+				return
+			}
 
-function onDocChange(e) {
-	if (applyingEdit) return
+			let doc = editor.document
+			let fullText = doc.getText()
+			let fullRange = new vscode.Range(doc.positionAt(0), doc.positionAt(fullText.length))
 
-	const editor = vscode.window.activeTextEditor
-	if (!editor) return
-	if (e.document.uri.toString() !== editor.document.uri.toString()) return
-
-	for (let i = 0; i < e.contentChanges.length; i++) {
-		const change = e.contentChanges[i]
-		if (change.text.length === 0) continue
-
-		const endPos = change.range.start.translate(0, change.text.length)
-		const startChar = Math.max(0, endPos.character - 4)
-		const startPos = new vscode.Position(endPos.line, startChar)
-		const last4Range = new vscode.Range(startPos, endPos)
-		const last4 = e.document.getText(last4Range)
-
-		if (last4 === "    " || last4 === "\t\t\t\t") {
-			applyingEdit = true
-			editor.edit(edit => {
-				edit.replace(last4Range, "\t")
-			}).finally(() => {
-				applyingEdit = false
+			// Replace runs of 4 spaces or 4 tabs with tabs
+			let replaced = fullText.replace(/(?: {4})+|(?:\t{4})+/g, (m) => {
+				let groups = m.length / 4
+				return "\t".repeat(groups)
 			})
+
+			if (replaced === fullText) {
+				vscode.window.showInformationMessage("Collapse to Tab: No changes needed")
+				return
+			}
+
+			await editor.edit((editBuilder) => {
+				editBuilder.replace(fullRange, replaced)
+			})
+
+			vscode.window.showInformationMessage("Collapse to Tab: Document updated")
 		}
-	}
+	)
+
+	context.subscriptions.push(disposable)
 }
 
-async function fixWholeDocument(editor) {
-	const doc = editor.document
-	const text = doc.getText()
-	const fullRange = new vscode.Range(doc.positionAt(0), doc.positionAt(text.length))
-
-	let replaced = text.replace(/\t{4}/g, "\t")
-	replaced = replaced.replace(/ {4}/g, "\t")
-
-	if (replaced === text) return
-
-	applyingEdit = true
-	await editor.edit(edit => {
-		edit.replace(fullRange, replaced)
-	})
-	applyingEdit = false
-}
-
-function deactivate() { }
+function deactivate() {}
 
 module.exports = { activate, deactivate }
